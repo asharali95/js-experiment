2e3813b74e35c53f105e4226a11fac3f6c7e063c
Ashar Ali <ashar.alia1999@gmail.com>
2021-03-16 17:09:22 +0500

    effect done

--- 3D Parallax Tilt Effect/index.js
+++ 3D Parallax Tilt Effect/index.js
@@ -1,3 +1,18 @@
-console.log("connected");
+var container = document.querySelector(".mouse-movement-area");
+var box = document.querySelector(".box");
+// console.log(container);
 
-var container 
+container.addEventListener("mousemove", (e) => {
+  console.log("entered");
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  box.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+container.addEventListener("mouseenter", (e) => {
+    box.style.transition = "none";
+  //Popout
+});
+container.addEventListener("mouseleave", (e) => {
+    box.style.transition = "all 0.5s ease";
+    box.style.transform = `rotateY(0deg) rotateX(0deg)`;
+})
